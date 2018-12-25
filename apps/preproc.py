2cae444f32756e6adafd8323ac9d0901ac6bb362
L2zz <ulla4571@gmail.com>
2018-12-25 13:54:59 +0000

    __temp__: feature scaling

--- apps/preproc.py
+++ apps/preproc.py
@@ -3,22 +3,28 @@ import sys
 import numpy as np
 import pandas as pd
 from PIL import Image
+from sklearn.preprocessing import minmax_scale
 
+# Input column: C, H, J, K, L, N, O, P, Q, R
+# Output column: W, X
 def get_data(file_name, is_train):
 
     file_path = '/assets/'
-    df = pd.read_csv(file_path + file_name, encoding='utf-8'i,\
+    df = pd.read_csv(file_path + file_name, encoding='utf-8', \
                      names=['A','B','C','D','E','F','G','H', \
                             'I','J','K','L','M','N','O','P', \
                             'Q','R','S','T','U','V','W','X'])
-    # Input: C, D, H, J, K, L, N, O, P, Q, R
-    # Output: W, X
-    df = df.drop(['A','B','E','F','G','I','M','S','T','U','V'], 1)
+    df = df.drop(['A','B','D','E','F','G','I','M','S','T','U','V'], 1)
 
     # Feature Scaling
+    # C: budget |  P: revenue | Q: runtime
+    df[['C','P','Q']] = df[['C','P','Q']].apply(minmax_scale)
 
     # One-hot Encoding
+    
+    
 
-    return df
+    print(df)
+    return X, Y
 
-get_data('movie_train.csv', True)
+get_data('movie_test.csv', True)
