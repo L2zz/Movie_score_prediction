e72053c3ae5ba1902c94bd18675b9dfc0c42d602
L2zz <ulla4571@gmail.com>
2018-12-25 22:26:49 +0900

    __temp__: naming column and drop

--- apps/preproc.py
+++ apps/preproc.py
@@ -8,11 +8,13 @@ def get_data(file_name, is_train):
 
     file_path = '/assets/'
     df = pd.read_csv(file_path + file_name, encoding='utf-8'i,\
-                     names=['A','B','C','D','E','F']) 
+                     names=['A','B','C','D','E','F','G','H', \
+                            'I','J','K','L','M','N','O','P', \
+                            'Q','R','S','T','U','V','W','X'])
     # Input: C, D, H, J, K, L, N, O, P, Q, R
     # Output: W, X
-    df = df.drop(df.columns[[0,1,4,5,6,]], axis=1)
-    
+    df = df.drop(['A','B','E','F','G','I','M','S','T','U','V'], 1)
+
     # Feature Scaling
 
     # One-hot Encoding
